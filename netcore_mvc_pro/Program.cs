ccda68b5e339a3f9fa27df2d8fef7baae4614f18
杰 黄 <937802080@qq.com>
2018-04-25 14:30:36 +0800

    update by hj 2018-04-25 v1.0.0

--- netcore_mvc_pro/Program.cs
+++ netcore_mvc_pro/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace netcore_mvc_pro
+namespace erpcore
 {
     public class Program
     {
--- netcore_mvc_pro/Startup.cs
+++ netcore_mvc_pro/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace netcore_mvc_pro
+namespace erpcore
 {
     public class Startup
     {
